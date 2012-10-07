82a532c08ca607436ad43bca3e27cc4afa4a1d86
Francisco <f.thiene@gmail.com>
2012-10-07 13:38:49 -0300

    Using Tornado HTTP Server

--- src/ssh_server.py
+++ src/ssh_server.py
@@ -18,7 +18,6 @@ import os
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
-#from yourapplication import app
 
 # configuration
 DATABASE = 'database.db'
